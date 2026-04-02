1fa7d28802c5284896190ec057f67f8cdf7c354b
Hannan <hannansshah2004@gmail.com>
2026-04-02 12:39:36 -0400

    remove shared-ts flight position, moved to frontend/lib/shared

--- packages/shared-ts/src/flight/position.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const FlightPositionSchema = z.object({
-  latitude: z.number(),
-  longitude: z.number(),
-  altitude: z.number(),
-});
-
-export type FlightPosition = z.infer<typeof FlightPositionSchema>;
